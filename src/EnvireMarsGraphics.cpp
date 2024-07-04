b4bcdb1fc6be1898cdc07c2398a27487d5e3d188
mulo01 <mulo01@dfki.de>
2024-07-04 23:33:49 +0000

    Add Heightfield

--- src/EnvireMarsGraphics.cpp
+++ src/EnvireMarsGraphics.cpp
@@ -25,7 +25,6 @@
 #include <mars_interfaces/Logging.hpp>
 #include <mars_interfaces/MARSDefs.h>
 
-
 typedef envire::core::GraphTraits::vertex_descriptor VertexDesc;
 using namespace configmaps;
 namespace mars
@@ -105,6 +104,7 @@ namespace mars
             //GraphEventDispatcher::subscribe(envireGraph.get());
             GraphItemEventDispatcher<envire::core::Item<::envire::types::Link>>::subscribe(envireGraph.get());
             GraphItemEventDispatcher<envire::core::Item<::envire::types::Inertial>>::subscribe(envireGraph.get());
+            GraphItemEventDispatcher<envire::core::Item<::envire::types::geometry::Heightfield>>::subscribe(envireGraph.get());
             GraphItemEventDispatcher<envire::core::Item<::envire::types::geometry::Plane>>::subscribe(envireGraph.get());
             GraphItemEventDispatcher<envire::core::Item<::envire::types::geometry::Box>>::subscribe(envireGraph.get());
             GraphItemEventDispatcher<envire::core::Item<::envire::types::geometry::Capsule>>::subscribe(envireGraph.get());
@@ -285,6 +285,28 @@ namespace mars
 
         void EnvireMarsGraphics::itemAdded(const envire::core::TypedItemAddedEvent<envire::core::Item<::envire::types::Inertial>>& e)
         {
+        }
+
+        void EnvireMarsGraphics::itemAdded(const envire::core::TypedItemAddedEvent<envire::core::Item<::envire::types::geometry::Heightfield>>& e)
+        {
+            if (!graphics)
+            {
+                return;
+            }
+
+            auto& geometry = e.item->getData();
+            auto config = geometry.getFullConfigMap();
+
+            if (e.item->getTag() == "visual")
+            {
+                LOG_INFO("VISUAL Heightfield: add: %s", e.frame.c_str());
+                createVisual(config, e.frame);
+            } else if (e.item->getTag() == "collision")
+            {
+                LOG_INFO("COLLISION Heightfield: add: %s", e.frame.c_str());
+                config["physicmode"] = config["type"];
+                createCollision(config, e.frame);
+            }
         }
 
         void EnvireMarsGraphics::itemAdded(const envire::core::TypedItemAddedEvent<envire::core::Item<::envire::types::geometry::Plane>>& e)
